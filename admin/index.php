beb90cf09916ca1f4c9fa85bf04cd20add0c40ab
corinne1976 <corinne.tina@lepoles.com>
2017-10-06 14:26:39 +0200

    commit5
    fin du travail du vendredi

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,17 @@
+<?php require'connexion.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title>Admin : prénom nom</title>
+    <?php 
+    
+    $sql = $pdoCV->query("SELECT * FROM t_utilisateurs");
+    $ligne_utilisateur = $sql->fetch();
+      
+     ?>
+    <title>Admin : <?php echo($ligne_utilisateur['pseudo']); ?>rénom nom</title>
   </head>
   <body>
-    <h1>Admin</h1>
+    <h1><?php echo ($ligne_utilisateur['prenom']); ?>Admin</h1>
   </body>
 </html>
