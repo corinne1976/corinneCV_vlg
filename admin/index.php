0184e2d30b50b8340d92daf25085448c66d67032
corinne1976 <corinne.tina@lepoles.com>
2017-10-13 12:29:23 +0200

    commit 8

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,22 @@
 <?php require'connexion.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <?php 
-    
-    $sql = $pdoCV->query("SELECT * FROM t_utilisateurs");
-    $ligne_utilisateur = $sql->fetch();
-      
-     ?>
-    <title>Admin : <?php echo($ligne_utilisateur['pseudo']); ?>rénom nom</title>
-  </head>
-  <body>
-    <h1><?php echo ($ligne_utilisateur['prenom']); ?>Admin</h1>
-  </body>
+<head>
+  <meta charset="utf-8">
+  <?php 
+  
+  $sql = $pdoCV->query("SELECT * FROM t_utilisateurs");
+  $ligne_utilisateur = $sql->fetch();
+  
+  ?>
+  <title>Admin  : <?php echo($ligne_utilisateur['pseudo']); ?>prénom nom</title>
+</head>
+<body>
+  <h1>Admin du site cv de<?php echo($ligne_utilisateur['pseudo']); ?>Admin</h1>
+  <p>texte</p>
+  <hr>
+  
+  <h2>Les compétences</h2>
+  
+</body>
 </html>
